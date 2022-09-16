b9e564a79deb76c2decefe4419ae4d3dbb8d98c2
kausty24 <92876845+kausty24@users.noreply.github.com>
2022-09-16 20:14:29 +0530

    Added order_status sql entries

--- services.sql
+++ services.sql
@@ -8,3 +8,8 @@ insert into service values(default,"PC_MOBILE_REPAIR");
 insert into service values(default,"LAUNDRYMAN");
 insert into service values(default,"VEGETABLE_VENDOR");
 insert into service values(default,"SALON");
+
+INSERT INTO order_status values(default, "NEW");
+INSERT INTO order_status values(default, "BIDDING_UNDER_PROCESS");
+INSERT INTO order_status values(default, "PENDING");
+INSERT INTO order_status values(default, "COMPLETED");
